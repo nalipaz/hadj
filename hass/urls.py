2916e498cadf04c5c118e6fd048c525c2514191c
Nicholas Alipaz <nalipaz@labiomed.org>
2016-10-28 14:02:02 -0700

    jquery mobile

--- hass/urls.py
+++ hass/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'hass'
 urlpatterns = [
-    url(r'^$', views.Index.as_view(), name='index'),
+    # url(r'^$', views.Index.as_view(), name='index'),
     url(r'^(?P<dashboard_name>[a-zA-Z0-9-_]+)/$', views.Dashboard.as_view(), name='dashboard'),
 ]
 
